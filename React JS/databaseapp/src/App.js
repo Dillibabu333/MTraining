4d48dbfdb942c3d5b2c7b759b04f0b8cbd8d8db6
Kalikiri Dilli Babu <107249329+Dillibabu333@users.noreply.github.com>
2024-05-09 19:33:41 +1200

    update

--- React JS/databaseapp/src/App.js
+++ React JS/databaseapp/src/App.js
@@ -2,20 +2,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./Components/Register";
 import Login from "./Components/Login";
 import Home from "./Components/Home";
+import ProductForm from "./ProductForm";
 
 function App() {
   
 
   return (
     <div>
-    <BrowserRouter>
-  
+    {/* <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login/>}/>
       <Route path="/register" element={<Register/>}/>
       <Route path="/home" element={<Home/>}/>
     </Routes>
-   </BrowserRouter>  
+   </BrowserRouter>   */}
+   <ProductForm/>
     </div>
   );
 }
